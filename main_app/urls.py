45f8bc13d356e6765357423432e5d71638ceb894
R <50966008+rashmika13@users.noreply.github.com>
2020-08-23 15:09:21 -0400

    add resources form

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('resources/', views.resources_index, name='index'),
+
+
     path('accounts/signup/', views.signup, name='signup'),
     path('resources/create/',
          views.ResourceCreate.as_view(),
